db48bcfa97d1573bd3b6140345cac0755b27f25e
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 16:02:56 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -10,6 +11,14 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+    int number;
+    do{
+      System.out.println("Please enter a positive integer. Enter a negative integer to exit.");
+      number = input.nextInt();
+    }
+    while(number > 0);
+    System.out.println("All done");
+    }
   }
-}
+
